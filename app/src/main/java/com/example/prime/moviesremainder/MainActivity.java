f405870322b90a5ecae9fc6b637bf0ebe3879f76
mohan22 <jakkula4@gmail.com>
2015-11-04 23:11:16 +0530

    Created project

--- app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
+++ app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//TODO: use opening.json in rottentomatoes.com
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
