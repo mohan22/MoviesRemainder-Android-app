5c7d3bb60d3d21f8e850d8b8f433145ee315ec6a
mohan22 <jakkula4@gmail.com>
2015-11-09 23:44:47 +0530

    added json parsing

--- app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
+++ app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
@@ -2,18 +2,143 @@ package com.example.prime.moviesremainder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.io.InputStreamReader;
+import java.lang.Object;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 //TODO: use opening.json in rottentomatoes.com
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private String data;
+    private URL url = null;
+    private StringBuffer buffer;
+    private char[] chars;
+    private JSONObject json;
+    private String title;
+   // private String data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                BufferedReader reader = null;
+                try
+                {
+                    try {
+                        url = new URL("http://private-84e2da-themoviedb.apiary-proxy.com/3/movie/now_playing?api_key=3f3e051781bd957a06fa49b7382263a4");
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    buffer = new StringBuffer();
+                    int read;
+
+                    chars = new char[1024];
+                    try {
+                        while ((read = reader.read(chars)) != -1)
+                            buffer.append(chars, 0, read);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    data = buffer.toString();
+
+                }
+                finally {
+                    if (reader != null)
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                }
+
+                try {
+                    json = new JSONObject(data);
+
+                    title = (String) json.get("dates");
+                    Log.d("1", title);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        Log.d("12", "testss");
+
+//        BufferedReader reader = null;
+//        try
+//        {
+//            try {
+//                url = new URL("http://private-84e2da-themoviedb.apiary-proxy.com/3/movie/now_playing?api_key=3f3e051781bd957a06fa49b7382263a4");
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            }
+//
+//            try {
+//                reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            buffer = new StringBuffer();
+//            int read;
+//
+//            chars = new char[1024];
+//            try {
+//                while ((read = reader.read(chars)) != -1)
+//                    buffer.append(chars, 0, read);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//         data = buffer.toString();
+//
+//         }
+//        finally {
+//            if (reader != null)
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//        }
+//
+//        try {
+//            json = new JSONObject(data);
+//
+//            title = (String) json.get("dates");
+//            Log.d("1", title);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
