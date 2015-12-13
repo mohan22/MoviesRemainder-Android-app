f6dfd490d8508b5786d6150d8fe1d02a2ffc3ebb
mohan22 <jakkula4@gmail.com>
2015-12-13 18:37:15 +0530

    Added internet connectivity Check.

--- app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
+++ app/src/main/java/com/example/prime/moviesremainder/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.prime.moviesremainder;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -13,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -73,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
             View line = new View(this);
             line.setBackgroundColor(Color.parseColor("#c0c0c0"));
-            line.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,2));
+            line.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, 2));
             myLayout.addView(line);
 
         }
@@ -84,158 +90,117 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         manager = new Manager();
 
         final TextView date=new TextView(this);
 
-        new Thread(new Runnable() {
-            public void run() {
+        if(!isOnline())
+        {
+            Log.d("net", "sdf");
+            // Toast.makeText(this, "Exiting..", Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("No Active Internet Connection is Detected!");
 
-                BufferedReader reader = null;
-                try
-                {
-                    try {
-                        url = new URL("http://private-84e2da-themoviedb.apiary-proxy.com/3/movie/now_playing?api_key=3f3e051781bd957a06fa49b7382263a4");
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
+//            alertDialogBuilder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface arg0, int arg1) {
+//                    Toast.makeText(MainActivity.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+//                }
+//            });
 
-                    try {
-                        reader = new BufferedReader(new InputStreamReader(url.openStream()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    buffer = new StringBuffer();
-                    int read;
-
-                    chars = new char[1024];
-                    try {
-                        while ((read = reader.read(chars)) != -1)
-                            buffer.append(chars, 0, read);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    data = buffer.toString();
-
+            alertDialogBuilder.setNegativeButton("OK",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
                 }
-                finally {
-                    if (reader != null)
+            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+
+        }
+
+        else
+        {
+            new Thread(new Runnable() {
+                public void run() {
+
+                    BufferedReader reader = null;
+                    try {
                         try {
-                            reader.close();
+                            url = new URL("http://private-84e2da-themoviedb.apiary-proxy.com/3/movie/now_playing?api_key=3f3e051781bd957a06fa49b7382263a4");
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            reader = new BufferedReader(new InputStreamReader(url.openStream()));
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                }
+                        buffer = new StringBuffer();
+                        int read;
 
-                try {
+                        chars = new char[1024];
+                        try {
+                            while ((read = reader.read(chars)) != -1)
+                                buffer.append(chars, 0, read);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-                    json = new JSONObject(data);
-                    title =  json.getJSONObject("dates").getString("minimum");
-                    Log.d("123", title);
+                        data = buffer.toString();
 
-
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//
-//                            date.setLayoutParams(lp);
-//                            date.setText(title);
-//                            myLayout.addView(date);
-//
-//
-//                        }
-//                    });
-
-                    jsArray = json.getJSONArray("results");
-
-
-
-
-
-                    for(int i=0;i<jsArray.length();i++)
-                    {
-                        json = jsArray.getJSONObject(i);
-                        movietitle=json.getString("title");
-                        Log.d("1235",movietitle);
-                        manager.moviesList.add(new Movie(json.getString("id"),"",json.getString("original_language"),movietitle,json.getString("release_date"),Double.parseDouble(json.getString("popularity")),Long.parseLong(json.getString("vote_count")),Double.parseDouble(json.getString("vote_average"))));
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                AddTextViewToList(movietitle);
-//                            }
-//                        });
-
-//                        try {
-//                            Thread.sleep(100);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
+                    } finally {
+                        if (reader != null)
+                            try {
+                                reader.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                     }
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            AddTextViewToList();
+                    try {
+
+                        json = new JSONObject(data);
+                        title = json.getJSONObject("dates").getString("minimum");
+                        Log.d("123", title);
+
+
+//
+
+                        jsArray = json.getJSONArray("results");
+
+                        for (int i = 0; i < jsArray.length(); i++) {
+                            json = jsArray.getJSONObject(i);
+                            movietitle = json.getString("title");
+                            Log.d("1235", movietitle);
+                            manager.moviesList.add(new Movie(json.getString("id"), "", json.getString("original_language"), movietitle, json.getString("release_date"), Double.parseDouble(json.getString("popularity")), Long.parseLong(json.getString("vote_count")), Double.parseDouble(json.getString("vote_average"))));
+//
                         }
-                    });
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AddTextViewToList();
+                            }
+                        });
 
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-
-            }
-        }).start();
+            }).start();
+        }
 
         Log.d("12", "testss");
 
-//        BufferedReader reader = null;
-//        try
-//        {
-//            try {
-//                url = new URL("http://private-84e2da-themoviedb.apiary-proxy.com/3/movie/now_playing?api_key=3f3e051781bd957a06fa49b7382263a4");
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            }
 //
-//            try {
-//                reader = new BufferedReader(new InputStreamReader(url.openStream()));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            buffer = new StringBuffer();
-//            int read;
-//
-//            chars = new char[1024];
-//            try {
-//                while ((read = reader.read(chars)) != -1)
-//                    buffer.append(chars, 0, read);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//         data = buffer.toString();
-//
-//         }
-//        finally {
-//            if (reader != null)
-//                try {
-//                    reader.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//        }
-//
-//        try {
-//            json = new JSONObject(data);
-//
-//            title = (String) json.get("dates");
-//            Log.d("1", title);
-//
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
@@ -258,5 +223,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
